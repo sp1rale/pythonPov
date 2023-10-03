16827f06d94a333a0ad646d2977ab967801dc697
sp1rale <dimayaremchyk24@gmail.com>
2023-10-03 14:23:19 +0300

    EX-3 task was done

--- main.py
+++ main.py
@@ -1,10 +1,2 @@
-num = int(input("Введіть число: "))
-
-factorial_sum = 0
-
-if num > 0:
-    for i in range(1, num + 1):
-        factorial_sum += i
-    print(f"Факторіал числа {num} дорівнює {factorial_sum}")
-else:
-    print("Будь ласка, введіть додатне число.")
+leng = int(input('Введіть довжину лінії: '))
+print(('=''-'* leng))
