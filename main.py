1780191e84cff455221ce80089f38061cc2e3e6c
sp1rale <dimayaremchyk24@gmail.com>
2023-10-03 13:58:27 +0300

    EX-2 task was done

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-start = int(input('Введіть початок діапазону'))
-end = int(input('Введіть кінець діапазону'))
+start = int(input('Введіть почаьок діапазону'))
+end = int(input('Ведіть кінець діапазону'))
 
-for num in range(start, end + 1):
-    if num % 7 == 0:
-        print(num)
+print(f"Всі числа в діапазоні: {list(range(start, end+1))}")
+
+print(f"Всі числа в діапазоні в спадному порядку: {list(range(end, start-1, -1))}")
+
+multiples_of_7 = [num for num in range(start, end+1) if num % 7 == 0]
+print(f"Числа, кратні 7: {multiples_of_7}")
+
+multiples_of_5 = len([num for num in range(start, end+1) if num % 5 == 0])
+print(f"Кількість чисел, кратних 5: {multiples_of_5}")
+
